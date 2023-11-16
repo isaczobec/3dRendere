dde84d570688b7baceab795255d9bf3289f13479
ISAC ZOBEC <izobec@ug.kth.se>
2023-11-16 16:18:22 +0100

    Rotate objects

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 import renderer as r
 import Objects3D as obj
+import Time
 
 class GameManager():
     def __init__(self,rendererObject: r.Renderer) -> None:
         self.renderer: r.Renderer = rendererObject
 
     def run(self):
-        pass
+        
+        if self.renderer.clickedObject != None:
+            self.renderer.clickedObject.Rotate(0,1*Time.deltaTime,0)
